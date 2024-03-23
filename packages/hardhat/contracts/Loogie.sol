d51414029dbc109968278cebc44ab61ac4619bc4
Simon <simonorugbo15@gmail.com>
2024-03-23 22:04:27 +0100

    Loogie Created Event

--- packages/hardhat/contracts/Loogie.sol
+++ packages/hardhat/contracts/Loogie.sol
@@ -46,6 +46,8 @@ contract Loogie is ILoogie, ERC721Enumerable, Ownable {
 
       mouthLength[id] = 180 + ((uint256(chubbiness[id] / 4) * uint256(uint8(predictableRandom[4]))) / 255);
 
+      emit LoogieCreated(color[id],chubbiness[id],mouthLength[id]);
+
       return id;
   }
   
--- packages/hardhat/contracts/interface/ILoogie.sol
+++ packages/hardhat/contracts/interface/ILoogie.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface ILoogie is IERC721 {
+    event LoogieCreated(bytes3 indexed color,uint256 indexed chubbiness ,uint256 indexed mouthLength);
     event MinterUpdated( address indexed minter);
     function mintItem() external returns (uint256);
     function burnItem(uint256 id) external;
