c1641122c469421e82984ba16a16ad7bc753875d
Simon <simonorugbo15@gmail.com>
2024-03-25 09:49:17 +0100

    Edit Subgraph

---
+++ packages/subgraph/src/mapping/loogie.ts
@@ -0,0 +1,56 @@
+import { LoogieCreated, Transfer, LoogieBurned } from "../types/Loogie/Loogie";
+import { Loogie, TransferEvent } from "../types/schema";
+import { getOrCreateAccount } from "../util/helper";
+import { log } from "@graphprotocol/graph-ts";
+import { BIGINT_ONE, BIGINT_ZERO } from "../util/constants";
+
+export function handleCreatedLoogie(event: LoogieCreated): void {
+  let loogieId = event.params.tokenId.toString();
+  let loogie = new Loogie(loogieId);
+  loogie.chubbiness = event.params.chubbiness;
+  loogie.color = event.params.color;
+  loogie.mouthLength = event.params.mouthLength;
+  loogie.owner = getOrCreateAccount(event.params.minter.toHexString()).id;
+
+  loogie.save();
+}
+
+export function handleBurnedLoogie(event: LoogieBurned): void {
+  let loogieId = event.params.tokenId.toString();
+  let loogie = Loogie.load(loogieId);
+  if (loogie == null) {
+    log.error("[handleBurnedLoogie] Loogie #{} not found. Hash: {}", [
+      loogieId,
+      event.transaction.hash.toHex(),
+    ]);
+    return;
+  }
+  loogie.unset(loogieId);
+}
+export function handleTransfer(event: Transfer): void {
+  // Get the sender and receiver accounts
+  let fromAccount = getOrCreateAccount(event.params.from.toHexString());
+  let toAccount = getOrCreateAccount(event.params.to.toHexString());
+  // Get the transferred Loogie ID
+  let transferredLoogieId = event.params.tokenId.toString();
+  // Create a new TransferEvent entity
+  let transferEvent = new TransferEvent(
+    event.transaction.hash.toHexString() + "_" + transferredLoogieId
+  );
+  transferEvent.blockNumber = event.block.number;
+  transferEvent.blockTimestamp = event.block.timestamp;
+  transferEvent.loogie = transferredLoogieId;
+  transferEvent.previousHolder = fromAccount.id;
+  transferEvent.newHolder = toAccount.id;
+  transferEvent.save();
+
+  if (fromAccount.tokenBalance < BIGINT_ZERO) {
+    log.error("Negative balance on holder {} with balance {}", [
+      fromAccount.id,
+      fromAccount.tokenBalance.toString(),
+    ]);
+  }
+
+  fromAccount.tokenBalance = fromAccount.tokenBalance.minus(BIGINT_ONE);
+  toAccount.tokenBalance = toAccount.tokenBalance.plus(BIGINT_ONE);
+}
--- packages/subgraph/src/mapping/loogieAuction.ts
+++ packages/subgraph/src/mapping/loogieAuction.ts
@@ -1,68 +1,14 @@
-import { BigInt, Address } from "@graphprotocol/graph-ts";
-import { LoogieCreated, Transfer } from "./types/Loogie/Loogie";
-import { Loogie, TransferEvent, Bid, Auction, Account } from "./types/schema";
-import { getOrCreateAccount } from "./util/helper";
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Loogie, Bid, Auction } from "../types/schema";
+import { getOrCreateAccount } from "../util/helper";
 import {
   AuctionBid,
   AuctionCreated,
   AuctionExtended,
   AuctionSettled,
-} from "./types/LoogieAuction/LoogieAuction";
+} from "../types/LoogieAuction/LoogieAuction";
 import { log } from "@graphprotocol/graph-ts";
-
-export function handleCreatedLoogie(event: LoogieCreated): void {
-  let loogieId = event.params.tokenId.toString();
-
-  let loogie = Loogie.load(loogieId);
-  if (loogie == null) {
-    log.critical("[handleCreatedLoogie] Loogie #{} not found. Hash: {}", [
-      loogieId,
-      event.transaction.hash.toHex(),
-    ]);
-    return;
-  }
-  loogie.chubbiness = event.params.chubbiness;
-  loogie.color = event.params.color;
-  loogie.mouthLength = event.params.mouthLength;
-  loogie.save();
-}
-export function handleTransfer(event: Transfer): void {
-  // Get the sender and receiver accounts
-  let fromAccount = getOrCreateAccount(event.params.from.toHexString());
-  let toAccount = getOrCreateAccount(event.params.to.toHexString());
-
-  // Get the transferred Loogie ID
-  let transferredLoogieId = event.params.tokenId.toString();
-
-  // Load the Loogie entity
-  let loogie = Loogie.load(transferredLoogieId);
-
-  // Check if the Loogie exists
-  if (loogie == null) {
-    log.error("[handleTransfer] Loogie #{} not found. Hash: {}", [
-      transferredLoogieId,
-      event.transaction.hash.toHex(),
-    ]);
-    return;
-  }
-
-  // Update ownership status
-  loogie.owner = toAccount.id;
-
-  // Save changes to the Loogie entity
-  loogie.save();
-
-  // Create a new TransferEvent entity
-  let transferEvent = new TransferEvent(
-    event.transaction.hash.toHexString() + "_" + transferredLoogieId
-  );
-  transferEvent.blockNumber = event.block.number;
-  transferEvent.blockTimestamp = event.block.timestamp;
-  transferEvent.loogie = transferredLoogieId;
-  transferEvent.previousHolder = fromAccount.id;
-  transferEvent.newHolder = toAccount.id;
-  transferEvent.save();
-}
+import { ZERO_ADDRESS } from "../util/constants";
 
 export function handleAuctionCreated(event: AuctionCreated): void {
   const loogieId = event.params.loogieId.toString();
@@ -81,6 +27,7 @@ export function handleAuctionCreated(event: AuctionCreated): void {
   auction.amount = BigInt.fromI32(0);
   auction.startTime = event.params.startTime;
   auction.endTime = event.params.endTime;
+  auction.bidder = getOrCreateAccount(ZERO_ADDRESS).id;
   auction.settled = false;
   auction.save();
 }
