1a51fabf2b5645d7802f28111f1e15a0065203e4
Simon <simonorugbo15@gmail.com>
2024-03-31 22:56:40 +0100

    Fix Mobile Modal

--- packages/nextjs/components/loogies/Modal.tsx
+++ packages/nextjs/components/loogies/Modal.tsx
@@ -10,7 +10,7 @@ type ModalProps = {
 export const Modal: React.FC<ModalProps> = ({ title, onClose, children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-[20] bg-[#00000030] overflow-y-scroll ">
-      <div className="p-8 bg-base-100 relative my-4 w-[60%] border border-black">
+      <div className="p-8 bg-base-100 relative my-4 md:w-[60%] border border-black w-[90%]">
         <h3 className="text-xl mb-6 font-semibold">{title}</h3>
         <div>{children}</div>
         <button className="absolute top-3 right-6 p-2 mt-3 rounded-full border border-black" onClick={onClose}>
